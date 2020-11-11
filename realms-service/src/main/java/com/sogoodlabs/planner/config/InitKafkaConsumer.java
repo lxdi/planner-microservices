062bebe92706fd7d69ba285d03f871cb2cb3360d
lxdi <alexfordeal@gmail.com>
2020-11-12 01:21:47 +0300

    Configs

--- realms-service/src/main/java/com/sogoodlabs/planner/config/InitKafkaConsumer.java
+++ realms-service/src/main/java/com/sogoodlabs/planner/config/InitKafkaConsumer.java
@@ -20,38 +20,38 @@ public class InitKafkaConsumer {
 
     private static final Logger LOG = Logger.getLogger(InitKafkaConsumer.class.getName());
 
-    @PostConstruct
-    public void init(){
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "kafka:9092");
-        props.put("group.id", "Realms");
-        props.put("key.deserializer",
-                "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer",
-                "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-
-        LOG.info("Subscribing to the topic");
-        consumer.subscribe(Collections.singletonList("Realms"));
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-
-        // Runnable, return void, nothing, submit and run the task async
-        executor.submit(() -> {
-            try {
-                while (true) {
-                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMinutes(2));
-                    for (ConsumerRecord<String, String> record : records)
-                    {
-                        LOG.info("Event received - "+record.value());
-                    }
-                }
-            } finally {
-                consumer.close();
-            }
-        });
-
-    }
+//    @PostConstruct
+//    public void init(){
+//        Properties props = new Properties();
+//        props.put("bootstrap.servers", "kafka:9092");
+//        props.put("group.id", "Realms");
+//        props.put("key.deserializer",
+//                "org.apache.kafka.common.serialization.StringDeserializer");
+//        props.put("value.deserializer",
+//                "org.apache.kafka.common.serialization.StringDeserializer");
+//        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+//
+//        LOG.info("Subscribing to the topic");
+//        consumer.subscribe(Collections.singletonList("Realms"));
+//
+//        ExecutorService executor = Executors.newSingleThreadExecutor();
+//
+//        // Runnable, return void, nothing, submit and run the task async
+//        executor.submit(() -> {
+//            try {
+//                while (true) {
+//                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMinutes(2));
+//                    for (ConsumerRecord<String, String> record : records)
+//                    {
+//                        LOG.info("Event received - "+record.value());
+//                    }
+//                }
+//            } finally {
+//                consumer.close();
+//            }
+//        });
+//
+//    }
 
     @StreamListener("api-gateway-bm-in")
     public void handleGreetings(@Payload String message) {
